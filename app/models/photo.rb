9fcdf74a282051fd4f0b0ed3132bd780fbb27983
Charles <jslack2537@gmail.com>
2019-08-28 16:42:17 +0000

    added photo uploading through carrierwave gem

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,5 @@
-class Photo < ApplicationRecord
-   belongs_to :user
-   belongs_to :place
+class Photo < ActiveRecord::Base
+belongs_to :user
+belongs_to :place
+mount_uploader :picture, PictureUploader
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'places#index'
-  resources :places do
-    resources :comments, :photos, only: :create
-  end 
+   resources :places do
+    resources :photos, only: :create
+    resources :comments, only: :create
+  end
 end
