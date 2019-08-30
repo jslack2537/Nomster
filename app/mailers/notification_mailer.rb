c3528d7229856a0ebd8e2ae13001e092104fea7b
Charles <jslack2537@gmail.com>
2019-08-30 17:54:20 +0000

    added comment_mailer_text$html

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,2 +1,9 @@
 class NotificationMailer < ApplicationMailer
+  default from: "no-reply@nomsterapp.com"
+
+  def comment_added
+    mail(to: "jslack2537@gmail.com",
+          subject: "A comment has been added to your place")
+  end
+
 end
